04d96120515d3fc85deb14354e44d82922fa0a1d
ankitkushwahaji938-hub <ankitkushwahaji938@gmail.com>
2026-04-01 21:43:21 +0530

    Add files via upload

--- app/src / main/java/com/ankitstudypoint/app/models/ToolItem.kt
+++ app/src / main/java/com/ankitstudypoint/app/models/ToolItem.kt
@@ -1,1 +1,11 @@
+package com.ankitstudypoint.app.models
 
+data class ToolItem(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val emoji: String,
+    val url: String,
+    val colorRes: Int,
+    val isFeatured: Boolean = false
+)
